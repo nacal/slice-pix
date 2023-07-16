c3a41e9e3b1ba4731eff185473ea1e87f3bd60c3
nacal <nacal@pepabo.com>
2023-07-16 22:40:54 +0900

    add eslint-plugin-jsx-a11y

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
     'prettier',
+    'plugin:jsx-a11y/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
